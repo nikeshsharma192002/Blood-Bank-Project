ee1418ecb0fae68e2119b58abf43a8abf21cebd0
Nikesh Sharma <106308542+nikeshsharma192002@users.noreply.github.com>
2023-08-06 13:45:59 +0530

    Add files via upload

---
+++ blood bank project/src/main/java/MyPack/MyDb.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package MyPack;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Oops
+ */
+public class MyDb {
+    Connection con;
+    public Connection getCon(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bldbank","root","root");
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex);
+        } catch (SQLException ex) {
+            
+             System.out.println(ex);
+        }
+    return con;
+    }
+    
+}
